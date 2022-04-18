cc83c76032f46bbdf8d07a70229e3bc64245a9df
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-04-18 23:59:15 +0900

    Initial commit

---
+++ src/test/java/kr/co/bblackhun/dockerblog/DockerBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.bblackhun.dockerblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
