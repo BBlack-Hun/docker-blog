83fdaeb3dd645d76b524e12afb116cd88d7aca76
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-05-02 20:34:54 +0900

    Get Post By Id REST API

--- src/main/java/kr/co/bblackhun/dockerblog/post/controller/PostController.java
+++ src/main/java/kr/co/bblackhun/dockerblog/post/controller/PostController.java
@@ -26,4 +26,10 @@ public class PostController {
     public List<PostDto> getAllPosts() {
         return postService.getAllPosts();
     }
+
+    // get post by id
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostbyId(@PathVariable(name= "id") long id) {
+        return ResponseEntity.ok(postService.getpostById(id));
+    }
 }
