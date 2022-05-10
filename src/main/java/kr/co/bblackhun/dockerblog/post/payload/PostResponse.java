36ed16ee547eda3e6235bb99386e0a68f4304eaf
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-05-10 23:30:02 +0900

    Customizing Pagination API Response

---
+++ src/main/java/kr/co/bblackhun/dockerblog/post/payload/PostResponse.java
@@ -0,0 +1,19 @@
+package kr.co.bblackhun.dockerblog.post.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostResponse {
+    private List<PostDto> content;
+    private int pageNo;
+    private int pageSize;
+    private long totlaElements;
+    private int totalPages;
+    private boolean last;
+}
