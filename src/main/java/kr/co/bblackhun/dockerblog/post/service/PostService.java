4ff8b0577c56c95d7994320319ee790569fc4232
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-05-10 23:18:56 +0900

    Pagination Support for Get All Posts REST API

--- src/main/java/kr/co/bblackhun/dockerblog/post/service/PostService.java
+++ src/main/java/kr/co/bblackhun/dockerblog/post/service/PostService.java
@@ -8,7 +8,7 @@ public interface PostService {
 
     PostDto createPost(PostDto postDto);
 
-    List<PostDto> getAllPosts();
+    List<PostDto> getAllPosts(int page, int pageSize);
 
     PostDto getpostById(long id);
 
